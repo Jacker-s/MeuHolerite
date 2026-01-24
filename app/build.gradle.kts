5746efdd5ae15a32b5497b17668d2a037e38fd6e
Jacker-s <ssj53415170@gmail.com>
2026-01-24 19:19:51 -0300

    Novos Recuros:  Atualizaçoes via OTA.  Nova tela de boas vindas.  Notificaçoes de faltas.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.jack.meuholerite"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.5"
+        versionCode = 3
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
